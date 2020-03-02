42cdd0255ab3a0555d7bd2999dda60debce7315b
Vlali <54359026+Vlali@users.noreply.github.com>
2020-03-02 20:15:11 +0100

    kop

---
+++ kop_munka.ino
@@ -0,0 +1,29 @@
+#include <dht.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
+
+dht DHT;
+
+#define DHT11_PIN 7
+
+void setup(){
+  lcd.begin(16, 2);
+  lcd.backlight();
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  lcd.setCursor(0,0); 
+  lcd.print("Temp:");
+  lcd.setCursor(9,0);
+  lcd.print(DHT.temperature);
+//  lcd.print((char)223);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("Humidity:");
+  lcd.print(DHT.humidity);
+  lcd.print("%");
+  delay(50);
+}
